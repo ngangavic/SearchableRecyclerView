3e0b80bcfb3cd3371bc54613dc3eb079c464d88c
ngangavic <ngangavictor10@gmail.com>
2020-01-08 11:22:12 +0300

    main activity

---
+++ app/src/main/java/com/example/searchablerecyclerview/kotlin/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.searchablerecyclerview.kotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.searchablerecyclerview.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
